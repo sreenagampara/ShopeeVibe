499d69122afa02610792be6c408d939d21066322
sreekanth <sree.nagampara>
2022-03-01 00:01:24 +0530

    connceted creat new page to database now new user register working

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import register
+
+
+class registeradmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'email')
+
 
 # Register your models here.
+admin.site.register(register, registeradmin)
--- home/views.py
+++ home/views.py
@@ -1,23 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .form import contactform
+from .models import register
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/home-page')
+        else:
+            messages.info(request, "Invalid User Name Or Password")
+            return redirect('/home')
+    else:
+        return render(request, 'login.html')
 
 
 def creat(request):
-    return render(request, 'creat-new.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        # print(username)
+        register.objects.create(username=username, password=password, email=email)
+        print('user created')
+        return redirect('/home')
+    else:
+
+        return render(request, 'creat-new.html')
+
 
 def homepage(request):
     return render(request, 'home page.html')
 
 
 def orderform(request):
-     return render(request, 'oder form.html')
+    return render(request, 'oder form.html')
+
 
 def orderplaced(request):
     return render(request, 'oredr placed.html')
