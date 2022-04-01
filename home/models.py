d6e94e4b6f8b544eb30e9f757a7f2f10d6e761f5
sreekanth <sree.nagampara>
2022-04-01 21:57:20 +0530

    change models and debug value to false for clear bugg

--- home/models.py
+++ home/models.py
@@ -10,28 +10,28 @@ class register(models.Model):
 
 
 class product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=500)
     price = models.FloatField()
     Stock = models.IntegerField()
-    image = models.CharField(max_length=5000)
-    detail = models.CharField(max_length=1000, default='sume string')
+    image = models.CharField(max_length=500)
+    detail = models.CharField(max_length=5000, default='sume string')
     objects = models.Manager()
 
 
 class cart(models.Model):
     costumername = models.CharField(max_length=50)
-    productname = models.CharField(max_length=50)
+    productname = models.CharField(max_length=500)
     productprice = models.FloatField()
-    productdetails = models.CharField(max_length=1000)
-    productimage = models.CharField(max_length=5000, default='some string')
+    productdetails = models.CharField(max_length=5000)
+    productimage = models.CharField(max_length=500, default='some string')
     objects = models.Manager()
 
 
 class order_list(models.Model):
-    name = models.CharField(max_length=50)
-    fname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50, default='Not Available')
-    cname = models.CharField(max_length=50, default='Not Available')
+    name = models.CharField(max_length=500)
+    fname = models.CharField(max_length=500)
+    lname = models.CharField(max_length=500, default='Not Available')
+    cname = models.CharField(max_length=500, default='Not Available')
     contry = models.CharField(max_length=50)
     street = models.CharField(max_length=500)
     apartment = models.CharField(max_length=500)
@@ -39,10 +39,10 @@ class order_list(models.Model):
     state = models.CharField(max_length=50)
     postcode = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    email = models.CharField(max_length=50, default='Not Available')
-    productname = models.CharField(max_length=50, default='Not Available')
+    email = models.CharField(max_length=500, default='Not Available')
+    productname = models.CharField(max_length=5000, default='Not Available')
     productprice = models.FloatField(default=0)
-    productimage = models.CharField(max_length=5000, default='Not Available')
+    productimage = models.CharField(max_length=500, default='Not Available')
     shipping_methode = models.CharField(max_length=50, default='Not Available')
-    productdetails = models.CharField(max_length=500, default='Not available')
+    productdetails = models.CharField(max_length=5000, default='Not available')
     objects = models.Manager()
--- shopping/settings.py
+++ shopping/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v=jx=+qxd2pbp!^cfehuddm)!6&smv&12wbis!fca_p7fdyxu('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shopeevaibe.herokuapp.com', '127.0.0.1']
 
